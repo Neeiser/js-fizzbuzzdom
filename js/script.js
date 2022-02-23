e78e087c8d6a0b3608250ddc4a4e9758c203da0d
Neeiser <stortini.study@gmail.com>
2022-02-23 16:08:35 +0100

    Colors added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-/* 1: Scrivi un programma che stampi in console i numeri da 1 a 100. */
-/* 2: Multipli di 3 stampi=             Fizz. */
-/* 3: Multipli di 5 stampi=             Buzz. */
-/* 4: Multipli di 3 che di 5 stampi=    FizzBuzz. */
+/* -1: Scrivi un programma che stampi in console i numeri da 1 a 100. */
+/* -2: Multipli di 3 stampi=             Fizz. */
+/* -3: Multipli di 5 stampi=             Buzz. */
+/* -4: Multipli di 3 che di 5 stampi=    FizzBuzz. */
 /* 5: Appendi un elemento html con il numero o la stringa corretta. */
 /* 6: Stile differente a seconda del valore dell'indice per i vari multipli */
 
 let boxContainer = document.querySelector('.box-container');
 
 for (i = 1; i <= 100; i++) {
-    const item = document.createElement('div');
-    item.classList.add();
-    item.innerHTML = i;
+    const element = document.createElement('div');
+    element.classList.add('box-structure');
+    element.innerHTML = i;
 
 
     if ((i % 3 == 0) && (i % 5 == 0)) {
-        console.log("multiplo di 3 e 5");
+        element.classList.add('multy15');
     } else if (i % 5 == 0) {
-       console.log("multiplo di 5");
+        element.classList.add("multy5");
     } else if(i % 3 == 0) {
-        console.log("multiplo di 3");
+        element.classList.add("multy3");
     } else {
-        console.log(i);
+        element.classList.add("multy-none");
     }
+
+    boxContainer.append(element);
 }
