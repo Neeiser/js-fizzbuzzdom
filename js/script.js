c6f1eccee38d771488be18e9b2bcad656f64759d
Neeiser <stortini.study@gmail.com>
2022-02-23 16:10:57 +0100

    Goal 4+5: Done - Finished

--- js/script.js
+++ js/script.js
@@ -2,25 +2,28 @@
 /* -2: Multipli di 3 stampi=             Fizz. */
 /* -3: Multipli di 5 stampi=             Buzz. */
 /* -4: Multipli di 3 che di 5 stampi=    FizzBuzz. */
-/* 5: Appendi un elemento html con il numero o la stringa corretta. */
-/* 6: Stile differente a seconda del valore dell'indice per i vari multipli */
+/* -5: Appendi un elemento html con il numero o la stringa corretta. */
+/* -6: Stile differente a seconda del valore dell'indice per i vari multipli */
 
 let boxContainer = document.querySelector('.box-container');
 
 for (i = 1; i <= 100; i++) {
     const element = document.createElement('div');
     element.classList.add('box-structure');
-    element.innerHTML = i;
 
 
     if ((i % 3 == 0) && (i % 5 == 0)) {
-        element.classList.add('multy15');
+        element.classList.add('multy15')
+        element.innerHTML ='FizzBuzz';
     } else if (i % 5 == 0) {
         element.classList.add("multy5");
+        element.innerHTML ='Buzz';
     } else if(i % 3 == 0) {
         element.classList.add("multy3");
+        element.innerHTML ='Fizz';
     } else {
         element.classList.add("multy-none");
+        element.innerHTML = i;
     }
 
     boxContainer.append(element);
