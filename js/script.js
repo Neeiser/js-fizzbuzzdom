447a0ef6e27d44da934ef7fda237d4943b5e7227
Neeiser <stortini.study@gmail.com>
2022-02-23 15:11:15 +0100

    1: Program loop created - from 1 to 100.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 /* 1: Scrivi un programma che stampi in console i numeri da 1 a 100. */
-/* 2: Multipli di 3 stampi= “Fizz” al posto del numero */
-/* 3: Multipli di 5 stampi Buzz. */
-/* 4: Multipli di 3 che di 5 stampi FizzBuzz. */
+/* 2: Multipli di 3 stampi=             Fizz. */
+/* 3: Multipli di 5 stampi=             Buzz. */
+/* 4: Multipli di 3 che di 5 stampi=    FizzBuzz. */
 /* 5: Appendi un elemento html con il numero o la stringa corretta. */
 /* 6: Stile differente a seconda del valore dell'indice per i vari multipli */
 
-let boxContainer;
-let boxItem;
-
-for
+for (i = 1; i < 101; i++) {
+    console.log(i);
+}
